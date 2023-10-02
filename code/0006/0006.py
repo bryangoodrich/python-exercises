c3c235f0dc60ec50af602c61c534a23741df78b6
Bryan Goodrich <goodrich.bryan@gmail.com>
2023-10-02 01:45:54 +0000

    Adds 0006

---
+++ code/0006/0006.py
@@ -0,0 +1,24 @@
+# github.com/bryangoodrich/python-exercises
+# code/0006/0006.py
+
+def use_state(initial_state):
+    state = initial_state
+
+    def set_state(new_state):
+        nonlocal state
+        state["value"] = new_state
+
+    return state, set_state
+
+count, set_count = use_state({"value": 0})
+print(count["value"])  # Output: 0
+
+increment = lambda: set_count(count["value"] + 1)
+decrement = lambda: set_count(count["value"] - 1)
+
+increment()  # 1
+increment()  # 2
+decrement()  # 1
+
+print(count["value"])
+# 1
