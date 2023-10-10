b09fdce342d09b6c7a2e30cb3a62bd2bce1a0339
Bryan Goodrich <goodrich.bryan@gmail.com>
2023-10-10 15:38:24 +0000

    Adds 0014

---
+++ code/0014/0014.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+df = pd.read_csv(url)
+
+print("Description before filling missing values in Age")
+print(df.describe())
+#        PassengerId    Survived      Pclass         Age       SibSp
+# count   891.000000  891.000000  891.000000  891.000000  891.000000
+# mean    446.000000    0.383838    2.308642   29.699118    0.523008
+# std     257.353842    0.486592    0.836071   13.002015    1.102743
+
+print(f"Percent of Missing\n{round(df.isnull().mean()*100, 2)}")
+# Age 19.87%
+
+df.Age.fillna(df.Age.mean(), inplace=True)
+
+print("Description after filling missing values in Age")
+print(df.describe())
+#        PassengerId    Survived      Pclass         Age       SibSp
+# count   891.000000  891.000000  891.000000  714.000000  891.000000
+# mean    446.000000    0.383838    2.308642   29.699118    0.523008
+# std     257.353842    0.486592    0.836071   14.526497    1.102743
+
+age_by_class = df.groupby('Pclass')['Age'].mean()
+ax = age_by_class.plot(kind='bar', color='skyblue')
+ax.set_xticklabels(age_by_class.index, rotation=0)
+plt.xlabel('Passenger Class')
+plt.ylabel('Mean Age')
+plt.title('Mean Age by Passenger Class')
+plt.savefig("plot.png")
---
+++ code/0014/README.md
@@ -0,0 +1,7 @@
+Handling missing values is critical to any #dataengineering and #datascience work you do in #Python. Here is a brief example of the classic Titanic dataset you can explore. 
+
+Age has nearly 1 in 5 missing values. We can fill these with the average to get a more complete picture with just a few lines of code. 
+
+Would you like to see more advanced methods for imputing or filling missing values? How would you handle the missing values in Cabin or Embarked?
+
+<img src="../../static/0014.png" />
