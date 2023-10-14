032683990b9a7d314434f3351c505a3824b37c43
Bryan Goodrich <goodrich.bryan@gmail.com>
2023-10-14 23:32:29 +0000

    Adds 0019

---
+++ code/0019/0019.py
@@ -0,0 +1,19 @@
+import numpy as np
+import faiss
+
+num_vectors, dim = (10, 64)
+data = np.random.random((num_vectors, dim)).astype('float32')
+index = faiss.IndexFlatL2(dim)  # L2 distance is used
+index.add(data)
+faiss.write_index(index, "index.faiss")
+
+index = faiss.read_index("index.faiss")
+query_vector = np.random.random((1, dim)).astype('float32')
+distances, indices = index.search(query_vector, 3)
+nearest_3 = zip(distances[0], indices[0])
+
+for i, document in enumerate(nearest_3):
+    print(f"Document {i}: ({document[1]}, {document[0]})")
+# Document 0: (3, 8.181063652038574)
+# Document 1: (9, 8.26373291015625)
+# Document 2: (8, 9.569644927978516)
---
+++ code/0019/README.md
@@ -0,0 +1,7 @@
+Vector databases are the hot topic lately, as they can provide the storage and execution engine for various advanced ML/AI methods. I'll admit, it's a new area for me. But I took a stab at getting started!
+
+This snippet shows how easy it is to use the Facebook AI Similarity Search (FAISS) index library. Real data to come, but for now it's just random noise. Create an index of certain dimensions, make another random one and find the nearest 3 in the index. 
+
+How might you use such a tool? Have you used the faiss library? What vector databases have you tried?
+
+<img src="../../static/0019.png" />
