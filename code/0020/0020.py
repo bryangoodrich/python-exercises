3abc1f150e2fd4f2d7c654aa349eb604904c54c0
Bryan Goodrich <goodrich.bryan@gmail.com>
2023-10-14 23:32:34 +0000

    Adds 0020

---
+++ code/0020/0020.py
@@ -0,0 +1,43 @@
+# github.com/bryangoodrich/python-exercises
+# code/0019/0019.py
+import threading
+import time
+import schedule
+
+def hourly_job():
+    t = time.strftime("%H:%M:%S", time.gmtime(time.time()))
+    print(f"I'm running on thread {threading.current_thread()}")
+    print(f"{t}: Did a thing on the hour")
+
+def daily_job():
+    t = time.strftime("%H:%M:%S", time.gmtime(time.time()))
+    print(f"I'm running on thread {threading.current_thread()}")
+    print(f"{t} - Did a thing on the day")
+
+def job_thread(job):
+    thread = threading.Thread(target=job)
+    thread.start()
+
+if __name__ == "__main__":
+    schedule.every(10).seconds.do(job_thread, hourly_job)
+    schedule.every(60).seconds.do(job_thread, daily_job)
+    
+    t = time.strftime("%H:%M:%S", time.gmtime(time.time()))
+    print(f"{t} ~ Starting scheduler")
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
+
+# 22:38:28 ~ Starting scheduler
+# I'm running on thread <Thread(Thread-1 (hourly_job), started 140145422452480)>
+# 22:38:38: Did a thing on the hour
+# I'm running on thread <Thread(Thread-2 (hourly_job), started 140145422452480)>
+# 22:38:48: Did a thing on the hour
+# I'm running on thread <Thread(Thread-3 (hourly_job), started 140145422452480)>
+# 22:38:58: Did a thing on the hour
+# I'm running on thread <Thread(Thread-4 (hourly_job), started 140145422452480)>
+# 22:39:08: Did a thing on the hour
+# I'm running on thread <Thread(Thread-5 (hourly_job), started 140145422452480)>
+# 22:39:18: Did a thing on the hour
+# I'm running on thread <Thread(Thread-6 (daily_job), started 140145422452480)>
+# 22:39:28 - Did a thing on the day
---
+++ code/0020/README.md
@@ -0,0 +1,7 @@
+The Schedule #python library is a neat lighteweight scheduler you can use to build your own Airflow-like job orchestration.
+
+This is far from a full-featured tool, as the docs admit, but looking at their examples, it can handle quite a lot of flows.
+
+Have you heard of this library? How do you schedule your #dataengineering pipelines? What do you want to see next?
+
+<img src="../../static/0020.png" />
