5c90703e19897eb3b3e558324dce5ac1136285ef
Bryan Goodrich <goodrich.bryan@gmail.com>
2023-10-17 18:20:05 +0000

    Adds 0021

---
+++ code/0021/0021.py
@@ -0,0 +1,35 @@
+# github.com/bryangoodrich/python-exercises
+# code/0021/0021.py
+import pandas as pd
+import pandera as pa
+
+chk_length_3 = pa.Check(lambda v: len(v) >= 3 if isinstance(v, str) else False,
+    name = "length 3", element_wise = True)
+nonnegative = pa.Check(lambda v: v >= 0, name = "nonnegative", element_wise = True)
+schema = pa.DataFrameSchema({
+    'name': pa.Column(pa.String, nullable=False),
+    'plate': pa.Column(pa.String, nullable = True, checks = chk_length_3),
+    'distance': pa.Column(pa.Float, nullable = True, checks = nonnegative)
+})
+
+try:
+    df = pd.read_csv("rides.csv")
+    schema.validate(df)
+except pa.errors.SchemaError as e:
+    print(e)
+# on-nullable series 'name' contains null values:
+# 4    NaN
+# Name: name, dtype: object
+
+# <Schema Column(name=plate, type=DataType(str))> failed element-wise validator 0:
+# <Check length 3>
+# failure cases:
+#    index failure_case
+# 0      2             
+# 1      5            A
+
+# <Schema Column(name=distance, type=DataType(float64))> failed element-wise validator 0:
+# <Check nonnegative>
+# failure cases:
+#    index  failure_case
+# 0      3          -3.2
---
+++ code/0021/README.md
@@ -0,0 +1,7 @@
+Schema validation is significant, whether in #analytics or #dataengineering. Maybe you've heard about Pydantic, as its very common, but a recent training introduced me to Pandera. If you've ever done form validation in JavaScript using Formik and Yup, it has a very similar feel. 
+
+You just define your schema object with check constraints (as functions or lambdas) then validate. This can easily be integrated into a CI/CD pipeline and automated integration testing.
+
+What validation tools do you use in #python? Have you used Pandera? Are you running automated testing? Let me know in the comments below!
+
+<img src="../../static/0021.png" />
