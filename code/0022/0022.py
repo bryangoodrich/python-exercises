9359a65eb037841d00a452d6eae66b152252c6b3
Bryan Goodrich <goodrich.bryan@gmail.com>
2023-10-18 19:16:04 +0000

    Adds 0022

---
+++ code/0022/0022.py
@@ -0,0 +1,20 @@
+# github.com/bryangoodrich/python-exercises
+# code/0022/0022.py
+import pandas as pd
+import requests
+import plotly.express as px
+
+counties_url = "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
+unemployment_url = "https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv"
+df = pd.read_csv(unemployment_url, dtype={"fips": str, "unemp": float})
+counties = requests.get(counties_url, timeout=10).json()
+
+fig = px.choropleth(df, geojson=counties, 
+    locations='fips', color='unemp',
+    color_continuous_scale="Viridis",
+    range_color=(0, 12), scope="usa",
+    labels={'unemp':'unemployment rate'})
+fig = fig.update_layout(
+    margin={"r":0,"t":0,"l":0,"b":0}, 
+    coloraxis_colorbar={"title": "% Unemployment"})
+fig.write_image("map.png", width=1000, height=400)
---
+++ code/0022/README.md
@@ -0,0 +1,11 @@
+If you want to level up your #python analytics and #datascience skills, you need to know how to map geospatial data. Many innocuous data sets can be linked to points, lines, and polygons for visual display on a map. 
+
+The Plotly library makes this easy and with the fig.show command, you can generate an interactive display in seconds! This example is lifted from their documentation, but it was fun to explore geopandas and matplotlib alternatives, as well. 
+
+Have you done spatial analytics before? Do you want to see how to use geopandas and spatial projections? Let me know in the comments below!
+
+<img src="../../static/0022.png" />
+
+# Output
+
+<img src="map.png">
