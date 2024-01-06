7a4c75029cdd047f649b8d8da2678660e5290d35
Bryan Goodrich <goodrich.bryan@gmail.com>
2024-01-06 02:27:52 +0000

    Add 0086

--- code/0086/0086.py
+++ code/0086/0086.py
@@ -16,20 +16,15 @@ def is_negative(series):
 
 dataset = "data/invoices.parquet"
 df = pq.read_table(dataset).to_pandas()
-df.columns = df.columns.map(underscore)
+df.columns = df.columns.map(underscore)  # FIX COLUMNS
 
-# enrichment
+df['country'] = df.country.astype("category")
 df['invoice_cost'] = df.unit_price * df.quantity
-df['stock_number'] = (pd.to_numeric(df.stock_code, errors='coerce')
+df['stock_number'] = (pd
+    .to_numeric(df.stock_code, errors='coerce')
     .fillna(0)
     .astype("int64"))
 
-df['country'] = df.country.astype("category")
-
-
-# relationships / associations (stock code to description)
-
-# cut into categories (quantity? total sales?)
 breakpoints = [-999_999_999, 0, 25_000, 50_000, 100_000, 999_999_999]
 labels = ['negative', '0-25,000', '25,000-50,000', '50,000-100,000', '100,000+']
 df['invoice_cost_bracket'] = pd.cut(
@@ -45,18 +40,19 @@ df.invoice_cost_bracket.value_counts()
 # 50,000-100,000         1
 # 100,000+               1
 
+
+# Are customers unique per country? No
+customers = df.groupby("customer_id").country.nunique().reset_index()
+len(customers[customers.country > 1]) # 8
+
+# Are descriptions unique per stock code?
+# Often when negative, but not always!
+codes = df.groupby('stock_code').description.nunique().reset_index()
+(codes.description > 1).sum() / len(codes) * 100  # 32.5%
+
 # Exploration and analysis
 is_numeric(df.invoice_no).sum()  # 541_909 (100%)
 is_numeric(df.stock_code).sum()  # 487_234 (89.91%)
 
 df.unit_price.map(is_negative).sum()  #      2 (~0.00%)
 df.quantity.map(is_negative).sum()    # 10_624 (1.96%)
-
-# validation? -> Customer across country?
-# sequential? invoice number or invoice dates
-
-# analyze -> timing of sales (time zones?)
-# returns? 
-# analyze -> sales by country
-# analyze -> repeat customers, sales by customer
-# analyze -> inflation? 
