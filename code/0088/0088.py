fd3f9fac95c24565734773f60fa61ed88f8b2554
Bryan Goodrich <goodrich.bryan@gmail.com>
2024-01-10 03:37:50 +0000

    Add 0088

--- code/0088/0088.py
+++ code/0088/0088.py
@@ -1,73 +1,31 @@
-import pandas as pd
-import pyarrow.parquet as pq
-import re
+from nltk.util import ngrams 
+from nltk.tokenize import word_tokenize
 
-def underscore(word):
-    """ Just import inflection.underscore! """
-    word = re.sub(r"([A-Z]+)([A-Z][a-z])", r'\1_\2', word)
-    word = re.sub(r"([a-z\d])([A-Z])", r'\1_\2', word)
-    return word.replace("-", "_").lower()
+def read_txt(text: str) -> tuple[str, list[str]]:
+    words = text.strip().split()
+    label = words[0]
+    message = words[1:]
+    return label, message
 
-def is_numeric(series):
-    return series.str.contains('\d+$')
+def create_ngrams(words: list[str], n: int = 2) -> list[str]:
+     slices = (words[n-(n-i):len(words)-(n-i-1)] for i in range(n))
+     return zip(*slices)
 
-def is_negative(series):
-    return series < 0
-
-dataset = "data/invoices.parquet"
-df = pq.read_table(dataset).to_pandas()
-df.columns = df.columns.map(underscore)  # FIX COLUMNS
-
-df['country'] = df.country.astype("category")
-df['invoice_cost'] = df.unit_price * df.quantity
-df['stock_number'] = (pd
-    .to_numeric(df.stock_code, errors='coerce')
-    .fillna(0)
-    .astype("int64"))
-
-breakpoints = [-999_999_999, 0, 25_000, 50_000, 100_000, 999_999_999]
-labels = ['negative', '0-25,000', '25,000-50,000', '50,000-100,000', '100,000+']
-df['invoice_cost_bracket'] = pd.cut(
-    df.invoice_cost, 
-    breakpoints, 
-    labels=labels, 
-    ordered=True)
-
-
-# sequential? invoice number or invoice dates
-df = df.sort_values("invoice_date")
-gap = df.loc[df.invoice_date.diff() > pd.Timedelta(days=1)]
-
-df.groupby("country").invoice_cost.sum()
-
-df["invoice_year"] = df.invoice_date.dt.year
-dg = df.groupby(['country', 'invoice_year']).invoice_cost.sum().reset_index()
-dg.pivot(index='country', 
-    columns='invoice_year',
-    values='invoice_cost')
-# invoice_year               2010         2011
-# country                                     
-# Australia               1005.10   136072.170
-# Austria                  257.04     9897.280
-# Bahrain                  205.74      342.660
-# Belgium                 1809.91    39101.050
-# Brazil                     0.00     1143.600
-
-
-# analyze -> repeat customers, sales by customer
-
-def get_30day_customer_count(df, end_date):
-  start_date = end_date - pd.Timedelta(days=30)
-  
-  df_last_30 = df[(df['invoice_date'] >= start_date) &  
-                  (df['invoice_date'] <= end_date)]
-                  
-  unique_cust_30d = df_last_30['customer_id'].unique()
-  
-  num_cust_30d = len(unique_cust_30d)
-  
-  return num_cust_30d
-
-# Example usage:
-end_date = '2023-01-15'
-num_cust = get_30day_customer_count(df, end_date)
+if __name__ == "__main__":
+    with open("data/spam.txt") as fh:
+        words = [read_txt(line) for line in fh]
+    
+    label, tokens = words[100]
+    mygrams = list(create_ngrams(tokens, 3))
+    trigrams = list(ngrams(tokens, 3))
+    for foo, bar in zip(mygrams, trigrams):
+        print(f"{foo} == {bar}")
+    # ('Please', "don't", 'text') == ('Please', "don't", 'text')
+    # ("don't", 'text', 'me') == ("don't", 'text', 'me')
+    # ('text', 'me', 'anymore.') == ('text', 'me', 'anymore.')
+    # ('me', 'anymore.', 'I') == ('me', 'anymore.', 'I')
+    # ('anymore.', 'I', 'have') == ('anymore.', 'I', 'have')
+    # ('I', 'have', 'nothing') == ('I', 'have', 'nothing')
+    # ('have', 'nothing', 'else') == ('have', 'nothing', 'else')
+    # ('nothing', 'else', 'to') == ('nothing', 'else', 'to')
+    # ('else', 'to', 'say.') == ('else', 'to', 'say.')
